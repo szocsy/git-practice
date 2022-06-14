ac7b6bc22efc1db2befcd5b4d041b904cf68816e
Ferenc Szécsy <szocsy@gmail.com>
2022-06-14 16:26:38 +0200

    c3

--- gitpractice/src/Greet.java
+++ gitpractice/src/Greet.java
@@ -1,7 +1,7 @@
 public class Greet {
 
     public void greet(String name) {
-        System.out.println("Hello, " + name + "!");
+        System.out.println("Greetings, " + name + "!");
     }
 
 }
